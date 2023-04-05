f06bc45a9497dafe674bf45ab2e023ebbe140b62
Chrism982 <chrism9822@gmail.com>
2023-04-05 18:33:17 +0200

    add offer subpage

--- src/js/main.js
+++ src/js/main.js
@@ -2,39 +2,36 @@ const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
 
-const msgStatus = document.querySelector('.msg-status')
-
-console.log(document.location.search);
-
-if (document.location.search === '?mail_status=sent') {
-	msgStatus.classList.add('success')
-	msgStatus.textContent = 'Wiadomość wysłana!'
-
-	setTimeout(() => {
-		msgStatus.classList.remove('success')
-	}, 3000);
-}
-
-if (document.location.search === '?mail_status=error') {
-	msgStatus.classList.add('error')
-	msgStatus.textContent = 'Wystąpił błąd.'
-
-	setTimeout(() => {
-		msgStatus.classList.remove('error')
-	}, 3000);
-}
+const msgStatus = document.querySelector('.msg-status');
 
 const handleNav = () => {
-    navBtn.classList.toggle('is-active')
-    navMobile.classList.toggle('nav-mobile--active')
-
-}
+	navBtn.classList.toggle('is-active');
+	navMobile.classList.toggle('nav-mobile--active');
+};
 
 navBtn.addEventListener('click', handleNav);
 
 const handleCurrentYear = () => {
-    const year = (new Date).getFullYear();
-    footerYear.innerText = year;
-}
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
 
 handleCurrentYear();
+// MESSAGE STATUS
+if (document.location.search === '?mail_status=sent') {
+	msgStatus.classList.add('success');
+	msgStatus.textContent = 'Wiadomość wysłana!';
+
+	setTimeout(() => {
+		msgStatus.classList.remove('success');
+	}, 3000);
+}
+
+if (document.location.search === '?mail_status=error') {
+	msgStatus.classList.add('error');
+	msgStatus.textContent = 'Wystąpił błąd.';
+
+	setTimeout(() => {
+		msgStatus.classList.remove('error');
+	}, 3000);
+}
